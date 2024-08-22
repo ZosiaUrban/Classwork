702ce7e9934a1e26628261d34d90abd521237a0a
ZosiaUrban <Zofia.Urban@metropolia.fi>
2024-08-22 14:31:42 +0300

    classwork questions

---
+++ randommath.py
@@ -0,0 +1,24 @@
+import random
+
+num1 = random.randint(0,9)
+
+
+num2 = random.randint(0,9)
+
+
+num3 = random.randint(0,9)
+
+
+print("your random code is", num1,num2,num3)
+
+
+num4 = random.randint(1,6)
+
+num5 = random.randint(1,6)
+
+
+num6 = random.randint(1,6)
+
+num7 = random.randint(1,6)
+
+print("your random code is", num4,num5,num6,num7)
